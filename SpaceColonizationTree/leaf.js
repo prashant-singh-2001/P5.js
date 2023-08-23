47f21253c2fa85c011ae3e3fd2c95ac3fc1311d4
prashant-singh-2001 <ps47600@gmail.com>
2023-08-23 11:14:27 +0530

    New Colonization

--- SpaceColonizationTree/leaf.js
+++ SpaceColonizationTree/leaf.js
@@ -1,9 +1,10 @@
 function Leaf() {
-  this.pos = createVector(random(width), random(height) - 100);
+  this.pos = createVector(random(width), random(height - 50));
   this.reached = false;
+
   this.show = function () {
-    noStroke();
     fill(255);
+    noStroke();
     ellipse(this.pos.x, this.pos.y, 4, 4);
   };
 }
--- SpaceColonizationTree/sketch.js
+++ SpaceColonizationTree/sketch.js
@@ -1,11 +1,14 @@
 var tree;
-var max_dist = 10;
+var max_dist = 200;
 var min_dist = 10;
+
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1600, 900);
   tree = new Tree();
 }
+
 function draw() {
   background(51);
   tree.show();
+  tree.grow();
 }
