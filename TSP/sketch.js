2ff1d19660772bb166116a43bdf75b1e58513db1
prashant-singh-2001 <ps47600@gmail.com>
2023-08-29 20:55:37 +0530

    Made TSP Interesting

--- TSP/sketch.js
+++ TSP/sketch.js
@@ -2,45 +2,72 @@ var door_oe_death = [];
 var doors = 6;
 var deaths;
 var low_Death;
+var inputField;
+var updateButton;
+
 function setup() {
   createCanvas(800, 600);
-  for (var i = 0; i < doors; i++) {
-    door_oe_death[i] = createVector(random(width), random(height));
-  }
-  var d = calcDist(door_oe_death);
-  deaths = d;
-  low_Death = door_oe_death.slice();
+
+  inputField = createInput();
+  inputField.position(10, height + 10);
+
+  updateButton = createButton("Update Doors");
+  updateButton.position(inputField.x + inputField.width + 10, height + 10);
+  updateButton.mousePressed(updateDoors);
+
+  initializeDoors(doors);
 }
+
 function draw() {
   background(0);
+
   for (var i = 0; i < doors; i++) {
     ellipse(door_oe_death[i].x, door_oe_death[i].y, 8, 8);
   }
-  stroke(255);
-  strokeWeight(1);
+
+  stroke(0, 60, 150);
+  strokeWeight(0.9);
   noFill();
+
   beginShape();
   for (var i = 0; i < doors; i++) {
     vertex(door_oe_death[i].x, door_oe_death[i].y);
   }
   endShape();
+
   stroke(255, 10, 200);
-  strokeWeight(4);
+  strokeWeight(6);
   noFill();
+
   beginShape();
   for (var i = 0; i < doors; i++) {
     vertex(low_Death[i].x, low_Death[i].y);
   }
   endShape();
+
   var i = floor(random(door_oe_death.length));
   var j = floor(random(door_oe_death.length));
   swap(door_oe_death, i, j);
   var d = calcDist(door_oe_death);
+
   if (deaths > d) {
     deaths = d;
     low_Death = door_oe_death.slice();
     console.log(deaths);
   }
+}
+
+function initializeDoors(numDoors) {
+  doors = numDoors;
+  door_oe_death = [];
+
+  for (var i = 0; i < doors; i++) {
+    door_oe_death[i] = createVector(random(width), random(height));
+  }
+
+  var d = calcDist(door_oe_death);
+  deaths = d;
+  low_Death = door_oe_death.slice();
 }
 
 function swap(a, i, j) {
@@ -56,4 +83,11 @@ function calcDist(points) {
     sum += d;
   }
   return sum;
+}
+
+function updateDoors() {
+  var newDoors = parseInt(inputField.value());
+  if (!isNaN(newDoors)) {
+    initializeDoors(newDoors);
+  }
 }
